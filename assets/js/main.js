ae62794a56c94dd08f6dbf590d155d2c71e059a7
the-dagger <harshithdwivedi@gmail.com>
2016-09-29 23:15:44 +0530

    Increase maximum number of blogs to be shown

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ jQuery(document).ready(function($) {
         // how many entries do you want?
         // default: 4
         // valid values: any integer
-        limit: 4,
+        limit: 8,
 
         // the effect, which is used to let the entries appear
         // default: 'show'
